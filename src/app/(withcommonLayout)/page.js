4cb70cc482f71463d4b5a1426620a00afd2e9bbf
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-03-08 16:32:45 +0600

    Advance data fetching strategies with SSR

--- src/app/(withcommonLayout)/page.js
+++ src/app/(withcommonLayout)/page.js
@@ -1,7 +1,17 @@
 import ProductCard from "@/components/products/ProductCard";
+import Link from "next/link";
 
 const HomePage = async()=>{
-  const res = await fetch ("http://localhost:5000/products");
+  //SSG -> Static Side generation
+  // const res = await fetch ("http://localhost:5000/products", {
+  //   cache : "force-cache"
+  // });
+  // ISR -> Incremental static Regeneration
+  const res = await fetch ("http://localhost:5000/products", {
+    next: {
+      revalidate : 5 // eita sudu fetch howa data gulu jodi update hoy taile update korbe static data gula te kunu change korte parbe na
+    }
+  });
   const products = await res.json();
   console.log(products);
   return (
@@ -9,10 +19,15 @@ const HomePage = async()=>{
       <h1 className="text-center text-4xl my-5">
          Data Fetching, Caching , and Revalidating
       </h1>
-     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-[90%] mx-auto">
-      {products.map ((product)=>(
+     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-[90%] mx-auto my-10">
+      {products.slice(0,4).map ((product)=>(
         <ProductCard key={product.id} product = {product}/>
       ))}
+     </div>
+     <div className="text-center">
+      <Link href="/products" className="bg-blue-500 text-white py-2 px-5 rounded-lg shadow-md transition-all">
+        See more
+      </Link>
      </div>
     </div>
   )
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,9 +1,19 @@
+import ProductCard from "@/components/products/ProductCard";
 
-const ProductsPage = ()=>{
+const AllProductsPage =async ()=>{
+    const res = await fetch ("http://localhost:5000/products", {
+        cache : "no-store" //server side rendering
+    });
+   const products = await res.json();
     return (
         <div>
-            <h1 className="text-4xl text-center">This is products page</h1>
+            <h1 className="text-4xl text-center">All Products</h1>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-[90%] mx-auto my-10">
+                {products.map((product)=>(
+                    <ProductCard key = {product.id} product = {product}/>
+                ))}
+            </div>
         </div>
     )
 }
-export default ProductsPage;
+export default AllProductsPage;
